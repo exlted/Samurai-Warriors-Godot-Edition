9bd9c368610976dbb63bef7f7ad308aaa3a3956e
exlted <benk@exlted.com>
2024-04-04 14:17:45 -0500

    Add Health, Implement Attacking, Add Death

--- scripts/movement/Actor.cs
+++ scripts/movement/Actor.cs
@@ -6,6 +6,9 @@ namespace SamuraiWarriorGodotEdition.scripts.movement;
 public partial class Actor : Cell
 {
 	protected Grid Map;
+	protected int Health = 5;
+	protected int MaxHealth = 5;
+	protected int Damage = 1;
 	
 	public Actor() : base(Type.Actor)
 	{
@@ -25,8 +28,19 @@ public partial class Actor : Cell
 	public void Attack(Actor other)
 	{
 		GD.Print("Attacking");
+		other.Health -= Damage;
+		if (other.Health > 0) return;
+		other.Health = 0;
+		other.Die();
 	}
 
+	private void Die()
+	{
+		GD.Print("Died");
+		Visible = false;
+		Dispose();
+	}
+	
 	protected void Move(Vector2 newPosition)
 	{
 		Position = newPosition;
